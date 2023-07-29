a6dae86e3cdf517b1aafcbc350e9148ad31a0bc5
JoaoSilva <up201906478@up.pt>
2023-07-29 17:44:48 +0100

    More responsive design

--- js/Player.js
+++ js/Player.js
@@ -21,6 +21,29 @@ export default class Player{
 
         document.addEventListener("keydown",this.keydown);
         document.addEventListener("keyup",this.keyup);
+
+
+        // Find all buttons with class "arcade-button"
+        const buttons = document.querySelectorAll('.arcade-button');
+
+        // Add event listeners for button press events
+        buttons.forEach(button => {
+            button.addEventListener('mousedown', this.handleButtonPress);
+            button.addEventListener('touchstart', this.handleButtonPress);
+            button.addEventListener('mouseup', this.handleButtonRelease);
+            button.addEventListener('touchend', this.handleButtonRelease);
+            button.addEventListener('touchcancel', this.handleButtonRelease);
+        });
+ 
+        // Find the joystick element with class "arcade-joystick"
+        const joystick = document.querySelector('.arcade-joystick');
+ 
+        // Add event listener for joystick press event
+        joystick.addEventListener('mousedown', this.handleJoystickPress);
+        joystick.addEventListener('touchstart', this.handleJoystickPress);
+        document.addEventListener('mouseup', this.handleJoystickRelease);
+        document.addEventListener('touchend', this.handleJoystickRelease);
+        document.addEventListener('touchcancel', this.handleJoystickRelease);
     }
 
     draw(ctx){
@@ -79,5 +102,32 @@ export default class Player{
         if(event.code == "Space"){
             this.shootPressed = false;
         }
+    }
+
+
+    // Event handler for button press events
+    handleButtonPress = event => {
+        const button = event.target;
+        if (button.classList.contains('red')) {
+            this.leftPressed = true;
+        } else if (button.classList.contains('blue')) {
+            this.rightPressed = true;
+        }
+    }
+
+    // Event handler for button release events
+    handleButtonRelease = event => {
+        this.leftPressed = false;
+        this.rightPressed = false;
+    }
+
+    // Event handler for joystick press event
+    handleJoystickPress = () => {
+        this.shootPressed = true;
+    }
+
+    // Event handler for joystick release event
+    handleJoystickRelease = () => {
+        this.shootPressed = false;
     }
 }
